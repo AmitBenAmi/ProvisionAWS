90e90d41948ddd86caf6e41117d88eed48967f17
AmitBenAmi <benami.amit1@gmail.com>
2020-05-02 18:59:33 +0300

    Returning now the hostname and ip address

--- server/main.py
+++ server/main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
+def get_host_name():
+    return socket.gethostname()
+
+def get_host_ip_addr(hostname):
+    return socket.gethostbyname(hostname)
+
 @app.route('/')
 def home():
-    return 'Hello World!'
+    hostname = get_host_name()
+    ip_addr = get_host_ip_addr(hostname)
+    return f'Hostname: {hostname}.<br/>IP Address: {ip_addr}'
 
 app.run()

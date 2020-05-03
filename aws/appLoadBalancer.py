2013179cd1c9527cc07dc05f3acae2f9f82acc75
AmitBenAmi <benami.amit1@gmail.com>
2020-05-04 01:07:33 +0300

    Creating the application load balancer

---
+++ aws/appLoadBalancer.py
@@ -0,0 +1,35 @@
+class ApplicationLoadBalancer:
+    def __init__(self, elbv2_client, target_group_arn: str, container_name: str, container_port: int, name: str ='web_load_balancer'):
+        self.__client = elbv2_client
+        self.__target_group_arn = target_group_arn
+        self.__container_name = container_name
+        self.__container_port = container_port
+        self.__name = name
+
+    def definition(self):
+        return {
+            'targetGroupArn': self.__target_group_arn,
+            'containerName': self.__container_name,
+            'containerPort': self.__container_port
+        }
+    
+    def create(self):
+        response = self.__client.create_load_balancer(
+            Name=self.__name,
+            Subnets=[
+                'string',
+            ],
+            Scheme='internet-facing',
+            Tags=[
+                {
+                    'key': 'Applicant',
+                    'value': 'Amit Ben Ami'
+                },
+                {
+                    'key': 'Position',
+                    'value': 'DevOps Engineer'
+                }
+            ],
+            Type='application',
+            IpAddressType='ipv4'
+        )

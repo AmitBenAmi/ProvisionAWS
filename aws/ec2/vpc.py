4eb941456f46e984eae245c794151aeb74f9b9fa
AmitBenAmi <benami.amit1@gmail.com>
2020-05-04 02:11:05 +0300

    Adding the option to create a VPC with subnets

--- aws/ec2/vpc.py
+++ aws/ec2/vpc.py
@@ -1,3 +1,7 @@
+from common import Region
+from ec2 import AvailabilityZone
+from ec2 import Subnet
+
 class PrivateNetwork:
     def __init__(self, ec2_client, cidr: str ='10.0.0.0/16'):
         self.__client = ec2_client
@@ -5,3 +9,14 @@ class PrivateNetwork:
 
     def create(self):
         response = self.__client.create_vpc(CidrBlock=self.__cidr)
+        self.__id = response['Vpc']['VpcId']
+
+    def create_with_subnets(self):
+        self.create()
+
+        region = Region()
+        az = AvailabilityZone(self.__client, region.name)
+
+        for zone_id in az.zone_ids():
+            subnet = Subnet(self.__client, zone_id, self.__id)
+            subnet.create()

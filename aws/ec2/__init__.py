b31326a407cf9734864fc5735efc17614bf6d1e2
AmitBenAmi <benami.amit1@gmail.com>
2020-05-04 01:58:24 +0300

    Adding the Availability Zone

--- aws/ec2/__init__.py
+++ aws/ec2/__init__.py
@@ -1,3 +1,4 @@
 from .ec2Client import EC2Client
+from .az import AvailabilityZone
 from .appLoadBalancer import ApplicationLoadBalancer
 from .vpc import PrivateNetwork
---
+++ aws/ec2/az.py
@@ -0,0 +1,21 @@
+class AvailabilityZone:
+    def __init__(self, ec2_client, region_name: str):
+        self.__client = ec2_client
+        self.__region_name = region_name
+
+    def zone_ids(self):
+        return list(map(lambda zone: zone['ZoneId'], self.__zones))
+
+    def get_all_zones_for_region(self):
+        response = self.__client.describe_availability_zones(
+            Filters=[
+                {
+                    'Name': 'region-name',
+                    'Values': [
+                        self.__region_name
+                    ]
+                }
+            ]
+        )
+
+        self.__zones = response['AvailabilityZones']

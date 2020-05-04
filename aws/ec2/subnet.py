904fae64fdcb46553d7ccd8f5e380f7f5f746301
AmitBenAmi <benami.amit1@gmail.com>
2020-05-04 04:38:20 +0300

    Adding the availability zone

--- aws/ec2/subnet.py
+++ aws/ec2/subnet.py
@@ -1,8 +1,9 @@
 class Subnet:
-    def __init__(self, ec2_client, vpc_id: str, cidr: str ='10.0.0.0/24'):
+    def __init__(self, ec2_client, vpc_id: str, cidr: str ='10.0.0.0/24', availability_zone_id: str =None):
         self.__client = ec2_client
         self.__vpc_id = vpc_id
         self.__cidr = cidr
+        self.__availability_zone_id = availability_zone_id
     
     @property
     def cidr(self):
@@ -13,9 +14,16 @@ class Subnet:
         return self.__id
 
     def create(self):
-        response = self.__client.create_subnet(
-            CidrBlock=self.__cidr,
-            VpcId=self.__vpc_id
-        )
+        if self.__availability_zone_id:
+            response = self.__client.create_subnet(
+                AvailabilityZoneId=self.__availability_zone_id,
+                CidrBlock=self.__cidr,
+                VpcId=self.__vpc_id
+            )
+        else:
+            response = self.__client.create_subnet(
+                CidrBlock=self.__cidr,
+                VpcId=self.__vpc_id
+            )
 
         self.__id = response['Subnet']['SubnetId']

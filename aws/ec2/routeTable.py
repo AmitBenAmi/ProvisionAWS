446eb4aea2b7d8c6602f2b86683bea3f773e19cc
AmitBenAmi <benami.amit1@gmail.com>
2020-05-04 03:16:46 +0300

    Adding the id for route table

--- aws/ec2/routeTable.py
+++ aws/ec2/routeTable.py
@@ -2,6 +2,11 @@ class RouteTable:
     def __init__(self, ec2_client, vpc_id: str):
         self.__ec2_client = ec2_client
         self.__vpc_id = vpc_id
+
+    @property
+    def id(self):
+        return self.__id
     
     def create(self):
         response = self.__ec2_client.create_route_table(VpcId=self.__vpc_id)
+        self.__id = response['RouteTable']['RouteTableId']

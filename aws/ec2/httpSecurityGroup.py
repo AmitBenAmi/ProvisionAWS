86c23f5348384bcab5007580b55c422588f9625f
AmitBenAmi <benami.amit1@gmail.com>
2020-05-04 22:11:44 +0300

    Adding the egress bound

--- aws/ec2/httpSecurityGroup.py
+++ aws/ec2/httpSecurityGroup.py
@@ -1,9 +1,10 @@
 class HttpSecurityGroup:
-    def __init__(self, ec2_client, vpc_id: str, port: int =80):
+    def __init__(self, ec2_client, vpc_id: str, port: int =80, healthcheck_port: int =8080):
         self.__client = ec2_client
         self.__name = 'http-security-group'
         self.__vpc_id = vpc_id
         self.__port = port
+        self.__healthcheck_port = healthcheck_port
 
     @property
     def id(self):
@@ -19,6 +20,9 @@ class HttpSecurityGroup:
         self.__id = response['GroupId']
 
         self.__create_ingress()
+
+        vpc_default_group_id = self.__vpc_default_security_group()
+        self.__create_egress_for_healthcheck(egress_group_id=vpc_default_group_id)
     
     def __create_ingress(self):
         response = self.__client.authorize_security_group_ingress(
@@ -37,3 +41,40 @@ class HttpSecurityGroup:
                 }
             ]
         )
+    
+    def __create_egress_for_healthcheck(self, egress_group_id: str):
+        response = self.__client.authorize_security_group_egress(
+            GroupId=self.__id,
+            IpPermissions=[
+                {
+                    'FromPort': self.__healthcheck_port,
+                    'ToPort': self.__healthcheck_port,
+                    'IpProtocol': 'tcp',
+                    'UserIdGroupPairs': [
+                        {
+                            'GroupId': egress_group_id
+                        }
+                    ]
+                }
+            ]
+        )
+    
+    def __vpc_default_security_group(self):
+        response = self.__client.describe_security_groups(
+            Filters=[
+                {
+                    'Name': 'vpc-id',
+                    'Values': [
+                        self.__vpc_id
+                    ]
+                },
+                {
+                    'Name': 'group-name',
+                    'Values': [
+                        'default'
+                    ]
+                }
+            ]
+        )
+
+        return response['SecurityGroups']['GroupId']

efd8871a28ccbe1ad307aa4f3a137d6e57fef82e
AmitBenAmi <benami.amit1@gmail.com>
2020-05-04 03:21:47 +0300

    Adding the route table

--- aws/ec2/routeTable.py
+++ aws/ec2/routeTable.py
@@ -10,3 +10,10 @@ class RouteTable:
     def create(self):
         response = self.__ec2_client.create_route_table(VpcId=self.__vpc_id)
         self.__id = response['RouteTable']['RouteTableId']
+    
+    def create_route(self, igw_id: str, cidr: str ='0.0.0.0/0'):
+        response = self.__ec2_client.create_route(
+            DestinationCidrBlock=cidr,
+            GatewayId=igw_id,
+            RouteTableId=self.__id
+        )

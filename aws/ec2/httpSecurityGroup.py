5a8148950b61842eff3b43729417e2288bca012f
AmitBenAmi <benami.amit1@gmail.com>
2020-05-04 21:47:10 +0300

    Adding the ingress rule

--- aws/ec2/httpSecurityGroup.py
+++ aws/ec2/httpSecurityGroup.py
@@ -1,8 +1,9 @@
 class HttpSecurityGroup:
-    def __init__(self, ec2_client, vpc_id: str):
+    def __init__(self, ec2_client, vpc_id: str, port: int =80):
         self.__client = ec2_client
         self.__name = 'http-security-group'
         self.__vpc_id = vpc_id
+        self.__port = port
     
     def create(self):
         response = self.__client.create_security_group(
@@ -11,4 +12,25 @@ class HttpSecurityGroup:
             VpcId=self.__vpc_id
         )
 
+        self.__create_ingress()
+
         self.__id = response['GroupId']
+    
+    def __create_ingress(self):
+        response = self.__client.authorize_security_group_ingress(
+            FromPort=80,
+            GroupId=self.__id,
+            IpPermissions=[
+                {
+                    'FromPort': self.__port,
+                    'ToPort': self.__port,
+                    'IpProtocol': 'tcp',
+                    'IpRanges': [
+                        {
+                            'CidrIp': '0.0.0.0/0',
+                            'Description': 'HTTP access from all over the world'
+                        }
+                    ]
+                }
+            ]
+        )

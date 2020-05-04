05c746fbe1e1d854dd0b012845d9bfbc8e24fd8d
AmitBenAmi <benami.amit1@gmail.com>
2020-05-04 03:43:04 +0300

    Adding the public subnets

--- aws/elb/appLoadBalancer.py
+++ aws/elb/appLoadBalancer.py
@@ -1,9 +1,10 @@
 class ApplicationLoadBalancer:
-    def __init__(self, elbv2_client, target_group_arn: str, container_name: str, container_port: int, name: str ='web_load_balancer'):
+    def __init__(self, elbv2_client, target_group_arn: str, container_name: str, container_port: int, public_subnet_ids: list, name: str ='web_load_balancer'):
         self.__client = elbv2_client
         self.__target_group_arn = target_group_arn
         self.__container_name = container_name
         self.__container_port = container_port
+        self.__public_subnet_ids = public_subnet_ids
         self.__name = name
 
     def definition(self):
@@ -16,9 +17,7 @@ class ApplicationLoadBalancer:
     def create(self):
         response = self.__client.create_load_balancer(
             Name=self.__name,
-            Subnets=[
-                'string',
-            ],
+            Subnets=self.__public_subnet_ids,
             Scheme='internet-facing',
             Tags=[
                 {
